d7f14f6cfff732dbc237114ba52c81ef4d256770
Katerina <katrindocik@gmail.com>
2021-02-28 11:54:00 +0100

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+ if (typeof(sampleActivity) === 'string' && +sampleActivity > 0 && +sampleActivity <= 15)
+    return Math.ceil(Math.log( MODERN_ACTIVITY / +sampleActivity) * HALF_LIFE_PERIOD / Math.log(2)) || false;
+    else return false;
 };
